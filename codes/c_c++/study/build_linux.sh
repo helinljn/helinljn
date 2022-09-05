5469c29c6bdbc65a7b028123613473de4df98470
helin <helin@wegotime.com>
2022-09-05 17:57:24 +0800

    add '--target all -- -j $(nproc)' for build_linux.sh

--- codes/c_c++/study/build_linux.sh
+++ codes/c_c++/study/build_linux.sh
@@ -3,20 +3,20 @@
 if   [ "$1" = "debug" ]; then
     mkdir -p build/linux/x64-Debug && cd build/linux/x64-Debug
     cmake -DCMAKE_BUILD_TYPE=Debug ../../../
-    cmake --build .
+    cmake --build . --target all -- -j $(nproc)
 elif [ "$1" = "release" ]; then
     mkdir -p build/linux/x64-Release && cd build/linux/x64-Release
     cmake -DCMAKE_BUILD_TYPE=Release ../../../
-    cmake --build .
+    cmake --build . --target all -- -j $(nproc)
 elif [ "$1" = "all" ]; then
     mkdir -p build/linux/x64-Debug && cd build/linux/x64-Debug
     cmake -DCMAKE_BUILD_TYPE=Debug ../../../
-    cmake --build .
+    cmake --build . --target all -- -j $(nproc)
 
     cd ../../../
     mkdir -p build/linux/x64-Release && cd build/linux/x64-Release
     cmake -DCMAKE_BUILD_TYPE=Release ../../../
-    cmake --build .
+    cmake --build . --target all -- -j $(nproc)
 else
     echo "Usage:./build_linux.sh [debug | release | all]"
     exit
