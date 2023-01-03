d921b582546d59e7c4715c8c9614d5262b9a0de7
helin <helin@wegotime.com>
2023-01-03 11:48:17 +0800

    change build dir to .build

--- codes/c_c++/study/build_linux.sh
+++ codes/c_c++/study/build_linux.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 if   [ "$1" = "debug" ]; then
-    mkdir -p build/linux/x64-Debug && cd build/linux/x64-Debug
+    mkdir -p .build/linux/x64-Debug && cd .build/linux/x64-Debug
     cmake -DCMAKE_BUILD_TYPE=Debug ../../../
     cmake --build . --target all -- -j $(nproc)
 elif [ "$1" = "release" ]; then
-    mkdir -p build/linux/x64-Release && cd build/linux/x64-Release
+    mkdir -p .build/linux/x64-Release && cd .build/linux/x64-Release
     cmake -DCMAKE_BUILD_TYPE=Release ../../../
     cmake --build . --target all -- -j $(nproc)
 elif [ "$1" = "all" ]; then
-    mkdir -p build/linux/x64-Debug && cd build/linux/x64-Debug
+    mkdir -p .build/linux/x64-Debug && cd .build/linux/x64-Debug
     cmake -DCMAKE_BUILD_TYPE=Debug ../../../
     cmake --build . --target all -- -j $(nproc)
 
     cd ../../../
-    mkdir -p build/linux/x64-Release && cd build/linux/x64-Release
+    mkdir -p .build/linux/x64-Release && cd .build/linux/x64-Release
     cmake -DCMAKE_BUILD_TYPE=Release ../../../
     cmake --build . --target all -- -j $(nproc)
 else
--- codes/c_c++/study/projects/protocol/proto/create_proto.bat
+++ codes/c_c++/study/projects/protocol/proto/create_proto.bat
@@ -2,7 +2,7 @@
 
 for %%f in (*.proto) do (
     echo %%f
-    ..\..\..\build\Release\protoc318.exe %%f --cpp_out=..
+    ..\..\..\.build\Release\protoc318.exe %%f --cpp_out=..
 )
 
 pause
