1fa1691deabd363dc2777f21224307ef25ea6cd1
helin <helin@wegotime.com>
2023-03-08 14:40:19 +0800

    add server

--- codes/c_c++/server/init-3rd.bat
+++ codes/c_c++/server/init-3rd.bat
@@ -1,13 +1,22 @@
 @echo off
 
 set ROOT_INIT_DIR=%CD%
+set FMT_INIT_DIR=%ROOT_INIT_DIR%/3rd/fmt
 set ASIO_INIT_DIR=%ROOT_INIT_DIR%/3rd/asio
 set COMMON_INIT_DIR=%ROOT_INIT_DIR%/3rd/CppCommon
 set SERVER_INIT_DIR=%ROOT_INIT_DIR%/3rd/CppServer
 
 cd %ROOT_INIT_DIR%
+if not exist %FMT_INIT_DIR% (
+    git clone -b 9.1.0 https://github.com/fmtlib/fmt.git %FMT_INIT_DIR%
+
+    cd %FMT_INIT_DIR%
+    git log -1
+)
+
+cd %ROOT_INIT_DIR%
 if not exist %ASIO_INIT_DIR% (
-    git clone -b asio-1-12-branch https://github.com/chriskohlhoff/asio.git %ASIO_INIT_DIR%
+    git clone -b asio-1-22-2 https://github.com/chriskohlhoff/asio.git %ASIO_INIT_DIR%
 
     cd %ASIO_INIT_DIR%
     git log -1
--- codes/c_c++/server/init-3rd.sh
+++ codes/c_c++/server/init-3rd.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 ROOT_INIT_DIR=`pwd`
+FMT_INIT_DIR=$ROOT_INIT_DIR/3rd/fmt
 ASIO_INIT_DIR=$ROOT_INIT_DIR/3rd/asio
 COMMON_INIT_DIR=$ROOT_INIT_DIR/3rd/CppCommon
 SERVER_INIT_DIR=$ROOT_INIT_DIR/3rd/CppServer
 
 cd $ROOT_INIT_DIR
+if [ ! -d $FMT_INIT_DIR ]; then
+    git clone -b 9.1.0 https://github.com/fmtlib/fmt.git $FMT_INIT_DIR
+
+    cd $FMT_INIT_DIR
+    git log -1
+fi
+
+cd $ROOT_INIT_DIR
 if [ ! -d $ASIO_INIT_DIR ]; then
-    git clone -b asio-1-12-branch https://github.com/chriskohlhoff/asio.git $ASIO_INIT_DIR
+    git clone -b asio-1-22-2 https://github.com/chriskohlhoff/asio.git $ASIO_INIT_DIR
 
     cd $ASIO_INIT_DIR
     git log -1
