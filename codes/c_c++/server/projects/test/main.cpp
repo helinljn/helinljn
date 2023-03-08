a0caccb0cd6109ed7017739a6b120e4756abed78
helin <helin@wegotime.com>
2023-03-08 19:19:58 +0800

    update test/main.cpp

--- codes/c_c++/server/projects/test/main.cpp
+++ codes/c_c++/server/projects/test/main.cpp
@@ -1,13 +1,19 @@
+#include "fmt/format.h"
 #include "asio.hpp"
 #include "asio/ssl.hpp"
+#include "spdlog/spdlog.h"
+#include "tinyxml2.h"
+#include "json.h"
+#include "server/version.h"
+#include "common/version.h"
 
 int main(void)
 {
     static_assert(__cplusplus == 201703);
 
-    fprintf(stdout, "Hello, I am test project test.\n");
+    fmt::print("Hello, I am test project test.\n");
 
-    fprintf(stdout, "\nPress \'Enter\' to exit...\n");
+    fmt::print("\nPress \'Enter\' to exit...\n");
     while (std::getchar() != '\n');
 
     return 0;
