421fa8bdfea65998b15fdceb08e8a524c470d858
helin <helin@wegotime.com>
2023-03-16 11:46:36 +0800

    add lua.hpp

---
+++ codes/c_c++/server/projects/modules/lua/include/lua.hpp
@@ -0,0 +1,16 @@
+#ifndef __LUA_HPP__
+#define __LUA_HPP__
+
+#if defined(__cplusplus)
+extern "C" {
+#endif // defined(__cplusplus)
+
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+
+#if defined(__cplusplus)
+}
+#endif // defined(__cplusplus)
+
+#endif // __LUA_HPP__
--- codes/c_c++/server/projects/test/main.cpp
+++ codes/c_c++/server/projects/test/main.cpp
@@ -7,18 +7,7 @@
 #include "json.h"
 #include "zlib.h"
 
-#if defined(__cplusplus)
-extern "C" {
-#endif // defined(__cplusplus)
-
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
-
-#if defined(__cplusplus)
-}
-#endif // defined(__cplusplus)
-
+#include "lua.hpp"
 #include "LuaBridge/LuaBridge.h"
 
 #if defined(_MSC_VER)
