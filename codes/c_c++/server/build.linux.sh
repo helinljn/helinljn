b57c152e9cf6657cbe241057ae256a28e8d1738b
helin <helin@wegotime.com>
2023-07-28 19:16:05 +0800

    update build scripts

--- codes/c_c++/server/build.linux.sh
+++ codes/c_c++/server/build.linux.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 
+ROOT_DIR=`pwd`
+DEBUG_DIR=$ROOT_DIR/.build/linux/x64-Debug
+RELEASE_DIR=$ROOT_DIR/.build/linux/x64-Release
+
 if   [ "$1" = "debug" ]; then
-    mkdir -p .build/linux/x64-Debug
-    cd .build/linux/x64-Debug
+    mkdir -p $DEBUG_DIR
+    cd $DEBUG_DIR
 
     cmake -DCMAKE_BUILD_TYPE=Debug ../../../
     cmake --build . -j $(nproc)
 
-    cd ../../../
+    cd $ROOT_DIR
 elif [ "$1" = "release" ]; then
-    mkdir -p .build/linux/x64-Release
-    cd .build/linux/x64-Release
+    mkdir -p $RELEASE_DIR
+    cd $RELEASE_DIR
 
     cmake -DCMAKE_BUILD_TYPE=Release ../../../
     cmake --build . -j $(nproc)
 
-    cd ../../../
+    cd $ROOT_DIR
 elif [ "$1" = "all" ]; then
-    mkdir -p .build/linux/x64-Debug
-    cd .build/linux/x64-Debug
+    mkdir -p $DEBUG_DIR
+    cd $DEBUG_DIR
 
     cmake -DCMAKE_BUILD_TYPE=Debug ../../../
     cmake --build . -j $(nproc)
+    echo ""
+    echo ""
+    echo ""
 
-    cd ../../../
+    cd $ROOT_DIR
 
-    mkdir -p .build/linux/x64-Release
-    cd .build/linux/x64-Release
+    mkdir -p $RELEASE_DIR
+    cd $RELEASE_DIR
 
     cmake -DCMAKE_BUILD_TYPE=Release ../../../
     cmake --build . -j $(nproc)
 
-    cd ../../../
+    cd $ROOT_DIR
 else
     echo "Usage: ./build.linux.sh [debug | release | all]"
 fi
--- codes/c_c++/server/build.windows.bat
+++ codes/c_c++/server/build.windows.bat
@@ -1,39 +1,45 @@
 @echo off
 
-set vs_version="Visual Studio 16 2019"
+set VS_VERSION="Visual Studio 16 2019"
+set ROOT_DIR=%CD%
+set DEBUG_DIR=%ROOT_DIR%/.build/windows/x64-Debug
+set RELEASE_DIR=%ROOT_DIR%/.build/windows/x64-Release
 
 if %1 equ debug (
-    mkdir .build\windows\x64-Debug
-    cd .build\windows\x64-Debug
+    mkdir %DEBUG_DIR% 2>nul
+    cd %DEBUG_DIR%
 
-    cmake -G %vs_version% -A x64 -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug ../../../
+    cmake -G %VS_VERSION% -A x64 -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug ../../../
     cmake --build . --config Debug
 
-    cd ../../../
+    cd %ROOT_DIR%
 ) else if %1 equ release (
-    mkdir .build\windows\x64-Release
-    cd .build\windows\x64-Release
+    mkdir %RELEASE_DIR% 2>nul
+    cd %RELEASE_DIR%
 
-    cmake -G %vs_version% -A x64 -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_BUILD_TYPE=Release ../../../
+    cmake -G %VS_VERSION% -A x64 -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_BUILD_TYPE=Release ../../../
     cmake --build . --config Release
 
-    cd ../../../
+    cd %ROOT_DIR%
 ) else if %1 equ all (
-    mkdir .build\windows\x64-Debug
-    cd .build\windows\x64-Debug
+    mkdir %DEBUG_DIR% 2>nul
+    cd %DEBUG_DIR%
 
-    cmake -G %vs_version% -A x64 -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug ../../../
+    cmake -G %VS_VERSION% -A x64 -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug ../../../
     cmake --build . --config Debug
+    echo.
+    echo.
+    echo.
 
-    cd ../../../
+    cd %ROOT_DIR%
 
-    mkdir .build\windows\x64-Release
-    cd .build\windows\x64-Release
+    mkdir %RELEASE_DIR% 2>nul
+    cd %RELEASE_DIR%
 
-    cmake -G %vs_version% -A x64 -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_BUILD_TYPE=Release ../../../
+    cmake -G %VS_VERSION% -A x64 -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_BUILD_TYPE=Release ../../../
     cmake --build . --config Release
 
-    cd ../../../
+    cd %ROOT_DIR%
 ) else (
     echo "Usage: build.windows.bat [debug | release | all]"
 )
