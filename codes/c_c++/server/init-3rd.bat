0c29ac2f29054c09c267e1632293915b187485df
helin <helin@wegotime.com>
2023-08-04 14:58:18 +0800

    update init-3rd.sh|.bat scripts

--- codes/c_c++/server/init-3rd.bat
+++ codes/c_c++/server/init-3rd.bat
@@ -7,44 +7,63 @@ set PROTOBUF_INIT_DIR=%ROOT_INIT_DIR%/3rd/protobuf
 set GOOGLETEST_INIT_DIR=%ROOT_INIT_DIR%/3rd/googletest
 set POCO_INIT_DIR=%ROOT_INIT_DIR%/3rd/poco
 
+echo ------------------
+echo -- fmt
 cd %ROOT_INIT_DIR%
 if not exist %FMT_INIT_DIR% (
     git clone -b 9.1.0 https://github.com/fmtlib/fmt.git %FMT_INIT_DIR%
 ) else (
     cd %FMT_INIT_DIR%
     git checkout .
+    git fetch -p origin
+    git checkout 9.1.0
 )
 
+echo ------------------
+echo -- libevent
 cd %ROOT_INIT_DIR%
 if not exist %LIBEVENT_INIT_DIR% (
     git clone -b release-2.1.12-stable https://github.com/libevent/libevent.git %LIBEVENT_INIT_DIR%
 ) else (
     cd %LIBEVENT_INIT_DIR%
     git checkout .
+    git fetch -p origin
+    git checkout release-2.1.12-stable
 )
 
+echo ------------------
+echo -- protobuf
 cd %ROOT_INIT_DIR%
 if not exist %PROTOBUF_INIT_DIR% (
     git clone -b 3.18.x https://github.com/protocolbuffers/protobuf.git %PROTOBUF_INIT_DIR%
 ) else (
     cd %PROTOBUF_INIT_DIR%
     git checkout .
+    git fetch -p origin
+    git checkout 3.18.x
 )
 
+echo ------------------
+echo -- googletest
 cd %ROOT_INIT_DIR%
 if not exist %GOOGLETEST_INIT_DIR% (
     git clone -b release-1.12.1 https://github.com/google/googletest.git %GOOGLETEST_INIT_DIR%
 ) else (
     cd %GOOGLETEST_INIT_DIR%
     git checkout .
+    git fetch -p origin
+    git checkout release-1.12.1
 )
 
+echo ------------------
+echo -- poco
 cd %ROOT_INIT_DIR%
 if not exist %POCO_INIT_DIR% (
     git clone https://github.com/pocoproject/poco.git %POCO_INIT_DIR%
 ) else (
     cd %POCO_INIT_DIR%
     git checkout .
+    git fetch -p origin
     git pull
 )
 
--- codes/c_c++/server/init-3rd.sh
+++ codes/c_c++/server/init-3rd.sh
@@ -7,43 +7,62 @@ PROTOBUF_INIT_DIR=$ROOT_INIT_DIR/3rd/protobuf
 GOOGLETEST_INIT_DIR=$ROOT_INIT_DIR/3rd/googletest
 POCO_INIT_DIR=$ROOT_INIT_DIR/3rd/poco
 
+echo ------------------
+echo -- fmt
 cd $ROOT_INIT_DIR
 if [ ! -d $FMT_INIT_DIR ]; then
     git clone -b 9.1.0 https://github.com/fmtlib/fmt.git $FMT_INIT_DIR
 else
     cd $FMT_INIT_DIR
     git checkout .
+    git fetch -p origin
+    git checkout 9.1.0
 fi
 
+echo ------------------
+echo -- libevent
 cd $ROOT_INIT_DIR
 if [ ! -d $LIBEVENT_INIT_DIR ]; then
     git clone -b release-2.1.12-stable https://github.com/libevent/libevent.git $LIBEVENT_INIT_DIR
 else
     cd $LIBEVENT_INIT_DIR
     git checkout .
+    git fetch -p origin
+    git checkout release-2.1.12-stable
 fi
 
+echo ------------------
+echo -- protobuf
 cd $ROOT_INIT_DIR
 if [ ! -d $PROTOBUF_INIT_DIR ]; then
     git clone -b 3.18.x https://github.com/protocolbuffers/protobuf.git $PROTOBUF_INIT_DIR
 else
     cd $PROTOBUF_INIT_DIR
     git checkout .
+    git fetch -p origin
+    git checkout 3.18.x
 fi
 
+echo ------------------
+echo -- googletest
 cd $ROOT_INIT_DIR
 if [ ! -d $GOOGLETEST_INIT_DIR ]; then
     git clone -b release-1.12.1 https://github.com/google/googletest.git $GOOGLETEST_INIT_DIR
 else
     cd $GOOGLETEST_INIT_DIR
     git checkout .
+    git fetch -p origin
+    git checkout release-1.12.1
 fi
 
+echo ------------------
+echo -- poco
 cd $ROOT_INIT_DIR
 if [ ! -d $POCO_INIT_DIR ]; then
     git clone https://github.com/pocoproject/poco.git $POCO_INIT_DIR
 else
     cd $POCO_INIT_DIR
     git checkout .
+    git fetch -p origin
     git pull
 fi
