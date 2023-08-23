1d656dc568294b45cc8a1539c527540fdb619bb3
helin <helin@wegotime.com>
2023-08-23 16:09:44 +0800

    add PocoJWT for server

--- codes/c_c++/server/projects/test/main.cpp
+++ codes/c_c++/server/projects/test/main.cpp
@@ -9,6 +9,7 @@
 #include "Poco/Data/MySQL/MySQL.h"
 #include "Poco/Foundation.h"
 #include "Poco/JSON/JSON.h"
+#include "Poco/JWT/JWT.h"
 #include "Poco/Net/Net.h"
 #include "Poco/Net/NetSSL.h"
 #include "Poco/Redis/Redis.h"
