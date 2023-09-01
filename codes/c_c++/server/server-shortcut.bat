0bf6b4c8238c61c1d8e63026a138076204bd20bf
helin <helin@wegotime.com>
2023-09-01 13:17:45 +0800

    update server-shortcut.bat

--- codes/c_c++/server/server-shortcut.bat
+++ codes/c_c++/server/server-shortcut.bat
@@ -1,25 +1,44 @@
 @echo off
 
 set ROOT_DIR=%~dp0
+
+@rem server_d
 set SERVER_D=%ROOT_DIR%\server_d.sln
-set SERVER_R=%ROOT_DIR%\server_r.sln
 set TARGET_D=%ROOT_DIR%\.build\windows\x64-Debug\server.sln
+
+@rem server_r
+set SERVER_R=%ROOT_DIR%\server_r.sln
 set TARGET_R=%ROOT_DIR%\.build\windows\x64-Release\server.sln
 
+@rem poco_vs160
+set SERVER_POCO=%ROOT_DIR%\poco_vs160.sln
+set TARGET_POCO=%ROOT_DIR%\tools\poco_vs160\poco_vs160.sln
+
+@rem server_d
 if exist %SERVER_D% (
     del /f /q "%SERVER_D%"
-)
-
-if exist %SERVER_R% (
-    del /f /q "%SERVER_R%"
 )
 
 if exist %TARGET_D% (
     mklink "%SERVER_D%" "%TARGET_D%"
 )
 
+@rem server_r
+if exist %SERVER_R% (
+    del /f /q "%SERVER_R%"
+)
+
 if exist %TARGET_R% (
     mklink "%SERVER_R%" "%TARGET_R%"
+)
+
+@rem poco_vs160
+if exist %SERVER_POCO% (
+    del /f /q "%SERVER_POCO%"
+)
+
+if exist %TARGET_POCO% (
+    mklink "%SERVER_POCO%" "%TARGET_POCO%"
 )
 
 pause
