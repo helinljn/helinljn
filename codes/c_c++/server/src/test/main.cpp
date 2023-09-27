f3064367a3bd7e73b625fa105f61cbb98e382eb0
helin <helin@wegotime.com>
2023-09-27 16:42:26 +0800

    add signal_handler for test/main.cpp

--- codes/c_c++/server/src/test/main.cpp
+++ codes/c_c++/server/src/test/main.cpp
@@ -3,7 +3,6 @@
 
 #include "fmt/format.h"
 #include "util/poco.h"
-#include "util/stack_trace.h"
 
 #include "Poco/Crypto/Crypto.h"
 #include "Poco/Data/Data.h"
@@ -18,6 +17,51 @@
 #include "Poco/XML/XML.h"
 #include "Poco/Zip/Zip.h"
 
+#include "util/stack_trace.h"
+#include "util/DateTimeEx.h"
+#include "Poco/FileStream.h"
+#include "Poco/DateTimeFormatter.h"
+#include "Poco/Thread.h"
+#include "Poco/Process.h"
+#include <csignal>
+
+#define REGISTER_SIGNAL(sig)                             \
+    if (::signal(sig, signal_handler) != signal_handler) \
+        ::signal(sig, signal_handler)
+
+void signal_handler(int sig)
+{
+    // 保存当前调用栈信息
+    {
+        Poco::FileOutputStream fos;
+        fos.open("crash.log", std::ios::app);
+        fos << "-------------------------"
+            << POCO_DEFAULT_NEWLINE_CHARS
+            << "sig:  " << sig
+            << POCO_DEFAULT_NEWLINE_CHARS
+            << "tid:  " << Poco::Thread::currentOsTid()
+            << POCO_DEFAULT_NEWLINE_CHARS
+            << "pid:  " << Poco::Process::id()
+            << POCO_DEFAULT_NEWLINE_CHARS
+            << "time: " << Poco::DateTimeFormatter::format(Poco::DateTimeEx().utcLocal(), "%Y-%m-%d %H:%M:%S")
+            << POCO_DEFAULT_NEWLINE_CHARS
+            << "-------------------------"
+            << POCO_DEFAULT_NEWLINE_CHARS
+            << "------ stack trace ------"
+            << POCO_DEFAULT_NEWLINE_CHARS
+            << common::stack_trace().to_string()
+            << "-------------------------"
+            << POCO_DEFAULT_NEWLINE_CHARS
+            << POCO_DEFAULT_NEWLINE_CHARS
+            << POCO_DEFAULT_NEWLINE_CHARS;
+        fos.close();
+    }
+
+    // 恢复该信号默认处理，并重新发送该信号
+    ::signal(sig, SIG_DFL);
+    ::raise(sig);
+}
+
 int main(int argc, char** argv)
 {
     static_assert(__cplusplus == 201703);
@@ -28,6 +72,12 @@ int main(int argc, char** argv)
 
     testing::InitGoogleMock(&argc, argv);
     testing::InitGoogleTest(&argc, argv);
+
+    // 注册信号处理
+    REGISTER_SIGNAL(SIGILL);
+    REGISTER_SIGNAL(SIGFPE);
+    REGISTER_SIGNAL(SIGSEGV);
+    REGISTER_SIGNAL(SIGABRT);
 
     const int ret = RUN_ALL_TESTS();
 
