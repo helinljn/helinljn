411357ad990e92f030a1e6d2e8e0c5c9747fe247
helin <helin@wegotime.com>
2023-10-25 14:23:54 +0800

    update github mirror site

--- codes/c_c++/server/init-3rd.bat
+++ codes/c_c++/server/init-3rd.bat
@@ -10,10 +10,10 @@ echo ------------------
 echo -- fmt
 cd %ROOT_INIT_DIR%
 if not exist %FMT_INIT_DIR% (
-    git clone -b 9.1.0 https://521github.com/fmtlib/fmt.git %FMT_INIT_DIR%
+    git clone -b 9.1.0 https://hub.nuaa.cf/fmtlib/fmt.git %FMT_INIT_DIR%
 ) else (
     cd %FMT_INIT_DIR%
-    git remote set-url origin https://521github.com/fmtlib/fmt.git
+    git remote set-url origin https://hub.nuaa.cf/fmtlib/fmt.git
     git remote -v
     git checkout .
     git fetch -p origin
@@ -24,10 +24,10 @@ echo ------------------
 echo -- poco
 cd %ROOT_INIT_DIR%
 if not exist %POCO_INIT_DIR% (
-    git clone -b poco-1.12.5 https://521github.com/pocoproject/poco.git %POCO_INIT_DIR%
+    git clone -b poco-1.12.5 https://hub.nuaa.cf/pocoproject/poco.git %POCO_INIT_DIR%
 ) else (
     cd %POCO_INIT_DIR%
-    git remote set-url origin https://521github.com/pocoproject/poco.git
+    git remote set-url origin https://hub.nuaa.cf/pocoproject/poco.git
     git remote -v
     git checkout .
     git fetch -p origin
@@ -38,10 +38,10 @@ echo ------------------
 echo -- protobuf
 cd %ROOT_INIT_DIR%
 if not exist %PROTOBUF_INIT_DIR% (
-    git clone -b 3.19.x https://521github.com/protocolbuffers/protobuf.git %PROTOBUF_INIT_DIR%
+    git clone -b 3.19.x https://hub.nuaa.cf/protocolbuffers/protobuf.git %PROTOBUF_INIT_DIR%
 ) else (
     cd %PROTOBUF_INIT_DIR%
-    git remote set-url origin https://521github.com/protocolbuffers/protobuf.git
+    git remote set-url origin https://hub.nuaa.cf/protocolbuffers/protobuf.git
     git remote -v
     git checkout .
     git fetch -p origin
@@ -52,10 +52,10 @@ echo ------------------
 echo -- googletest
 cd %ROOT_INIT_DIR%
 if not exist %GOOGLETEST_INIT_DIR% (
-    git clone -b v1.12.x https://521github.com/google/googletest.git %GOOGLETEST_INIT_DIR%
+    git clone -b v1.12.x https://hub.nuaa.cf/google/googletest.git %GOOGLETEST_INIT_DIR%
 ) else (
     cd %GOOGLETEST_INIT_DIR%
-    git remote set-url origin https://521github.com/google/googletest.git
+    git remote set-url origin https://hub.nuaa.cf/google/googletest.git
     git remote -v
     git checkout .
     git fetch -p origin
