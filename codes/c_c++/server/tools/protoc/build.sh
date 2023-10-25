cb747ad39c549a254985a02f65a22def706327a2
helin <helin@wegotime.com>
2023-10-25 19:36:06 +0800

    update protoc/build.sh

--- codes/c_c++/server/tools/protoc/build.sh
+++ codes/c_c++/server/tools/protoc/build.sh
@@ -7,7 +7,7 @@ mkdir -p $PBMSG_DIR
 
 find $PBMSG_DIR -name "*.proto" | xargs ./protoc-exec --proto_path=$PBMSG_DIR --cpp_out=$PBMSG_DIR
 
-if [[ $KERNEL_VERSION == *Microsoft* ]]; then
+if [[ ${KERNEL_VERSION,,} == *microsoft* ]]; then
     find $PBMSG_DIR -name '*.pb.h'  | xargs unix2dos
     find $PBMSG_DIR -name '*.pb.cc' | xargs unix2dos
 fi
