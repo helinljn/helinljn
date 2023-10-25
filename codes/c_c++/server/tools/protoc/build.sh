af3dad1feb464295aab0a7ee9a177f4ebcb88116
helinljn <helinljn@163.com>
2023-10-25 18:56:35 +0800

    update protoc/build.sh

--- codes/c_c++/server/tools/protoc/build.sh
+++ codes/c_c++/server/tools/protoc/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 PBMSG_DIR=../../src/common/pbmsg
+KERNEL_VERSION=`uname -r`
 
 mkdir -p $PBMSG_DIR
 
 find $PBMSG_DIR -name "*.proto" | xargs ./protoc-exec --proto_path=$PBMSG_DIR --cpp_out=$PBMSG_DIR
-find $PBMSG_DIR -name '*.pb.h'  | xargs unix2dos
-find $PBMSG_DIR -name '*.pb.cc' | xargs unix2dos
+
+if [[ $KERNEL_VERSION == *Microsoft* ]]; then
+    find $PBMSG_DIR -name '*.pb.h'  | xargs unix2dos
+    find $PBMSG_DIR -name '*.pb.cc' | xargs unix2dos
+fi
