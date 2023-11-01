338489e02dfe4770795142dcf8e667133fb5042e
helin <helin@wegotime.com>
2023-11-01 15:52:31 +0800

    add tools/unix2dos

--- codes/c_c++/server/tools/protoc/build.bat
+++ codes/c_c++/server/tools/protoc/build.bat
@@ -5,7 +5,7 @@ set PBMSG_DIR=../../src/common/pbmsg
 mkdir "%PBMSG_DIR%" 2>nul
 
 for %%f in (%PBMSG_DIR%/*.proto) do (protoc-exec.exe --proto_path=%PBMSG_DIR% --cpp_out=%PBMSG_DIR% %%f)
-for %%f in (%PBMSG_DIR%/*.pb.h)  do (unix2dos.exe %PBMSG_DIR%/%%f)
-for %%f in (%PBMSG_DIR%/*.pb.cc) do (unix2dos.exe %PBMSG_DIR%/%%f)
+for %%f in (%PBMSG_DIR%/*.pb.h)  do (..\unix2dos\unix2dos.exe %PBMSG_DIR%/%%f)
+for %%f in (%PBMSG_DIR%/*.pb.cc) do (..\unix2dos\unix2dos.exe %PBMSG_DIR%/%%f)
 
 pause
