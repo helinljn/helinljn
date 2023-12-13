0ba647145fd96a2cf8896c0cf3b55fb0d2920b9a
helin <helin@wegotime.com>
2023-12-13 10:37:01 +0800

    update github mirror

--- codes/c_c++/server/init-3rd.bat
+++ codes/c_c++/server/init-3rd.bat
@@ -13,10 +13,10 @@ echo ------------------
 echo -- fmt
 cd %ROOT_INIT_DIR%
 if not exist %FMT_INIT_DIR% (
-    git clone -b 9.1.0 https://hub.njuu.cf/fmtlib/fmt.git %FMT_INIT_DIR%
+    git clone -b 9.1.0 https://hub.yzuu.cf/fmtlib/fmt.git %FMT_INIT_DIR%
 ) else (
     cd %FMT_INIT_DIR%
-    git remote set-url origin https://hub.njuu.cf/fmtlib/fmt.git
+    git remote set-url origin https://hub.yzuu.cf/fmtlib/fmt.git
     git remote -v
     git checkout .
     git fetch -p origin
@@ -27,10 +27,10 @@ echo ------------------
 echo -- poco
 cd %ROOT_INIT_DIR%
 if not exist %POCO_INIT_DIR% (
-    git clone -b poco-1.12.5 https://hub.njuu.cf/pocoproject/poco.git %POCO_INIT_DIR%
+    git clone -b poco-1.12.5 https://hub.yzuu.cf/pocoproject/poco.git %POCO_INIT_DIR%
 ) else (
     cd %POCO_INIT_DIR%
-    git remote set-url origin https://hub.njuu.cf/pocoproject/poco.git
+    git remote set-url origin https://hub.yzuu.cf/pocoproject/poco.git
     git remote -v
     git checkout .
     git fetch -p origin
@@ -41,10 +41,10 @@ echo ------------------
 echo -- protobuf
 cd %ROOT_INIT_DIR%
 if not exist %PROTOBUF_INIT_DIR% (
-    git clone -b 3.19.x https://hub.njuu.cf/protocolbuffers/protobuf.git %PROTOBUF_INIT_DIR%
+    git clone -b 3.19.x https://hub.yzuu.cf/protocolbuffers/protobuf.git %PROTOBUF_INIT_DIR%
 ) else (
     cd %PROTOBUF_INIT_DIR%
-    git remote set-url origin https://hub.njuu.cf/protocolbuffers/protobuf.git
+    git remote set-url origin https://hub.yzuu.cf/protocolbuffers/protobuf.git
     git remote -v
     git checkout .
     git fetch -p origin
@@ -55,10 +55,10 @@ echo ------------------
 echo -- googletest
 cd %ROOT_INIT_DIR%
 if not exist %GOOGLETEST_INIT_DIR% (
-    git clone -b v1.12.x https://hub.njuu.cf/google/googletest.git %GOOGLETEST_INIT_DIR%
+    git clone -b v1.12.x https://hub.yzuu.cf/google/googletest.git %GOOGLETEST_INIT_DIR%
 ) else (
     cd %GOOGLETEST_INIT_DIR%
-    git remote set-url origin https://hub.njuu.cf/google/googletest.git
+    git remote set-url origin https://hub.yzuu.cf/google/googletest.git
     git remote -v
     git checkout .
     git fetch -p origin
@@ -69,10 +69,10 @@ echo ------------------
 echo -- plthook
 cd %ROOT_INIT_DIR%
 if not exist %PLTHOOK_INIT_DIR% (
-    git clone https://hub.njuu.cf/kubo/plthook.git %PLTHOOK_INIT_DIR%
+    git clone https://hub.yzuu.cf/kubo/plthook.git %PLTHOOK_INIT_DIR%
 ) else (
     cd %PLTHOOK_INIT_DIR%
-    git remote set-url origin https://hub.njuu.cf/kubo/plthook.git
+    git remote set-url origin https://hub.yzuu.cf/kubo/plthook.git
     git remote -v
     git checkout .
     git fetch -p origin
@@ -83,10 +83,10 @@ echo ------------------
 echo -- distorm
 cd %ROOT_INIT_DIR%
 if not exist %DISTORM_INIT_DIR% (
-    git clone https://hub.njuu.cf/gdabah/distorm.git %DISTORM_INIT_DIR%
+    git clone https://hub.yzuu.cf/gdabah/distorm.git %DISTORM_INIT_DIR%
 ) else (
     cd %DISTORM_INIT_DIR%
-    git remote set-url origin https://hub.njuu.cf/gdabah/distorm.git
+    git remote set-url origin https://hub.yzuu.cf/gdabah/distorm.git
     git remote -v
     git checkout .
     git fetch -p origin
@@ -97,10 +97,10 @@ echo ------------------
 echo -- funchook
 cd %ROOT_INIT_DIR%
 if not exist %FUNCHOOK_INIT_DIR% (
-    git clone https://hub.njuu.cf/kubo/funchook.git %FUNCHOOK_INIT_DIR%
+    git clone https://hub.yzuu.cf/kubo/funchook.git %FUNCHOOK_INIT_DIR%
 ) else (
     cd %FUNCHOOK_INIT_DIR%
-    git remote set-url origin https://hub.njuu.cf/kubo/funchook.git
+    git remote set-url origin https://hub.yzuu.cf/kubo/funchook.git
     git remote -v
     git checkout .
     git fetch -p origin
