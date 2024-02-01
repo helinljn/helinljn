2fc73c52978a9c341d6d43ee0ba764133de11aed
helin <helin@wegotime.com>
2024-02-01 14:45:49 +0800

    remove PocoData.cmake & PocoDataMySQL.cmake & PocoRedis.cmake

--- codes/c_c++/server/tests/test/main.cpp
+++ codes/c_c++/server/tests/test/main.cpp
@@ -5,12 +5,9 @@
 #include "util/types.h"
 
 #include "Poco/Crypto/Crypto.h"
-#include "Poco/Data/Data.h"
-#include "Poco/Data/MySQL/MySQL.h"
 #include "Poco/Foundation.h"
 #include "Poco/Net/Net.h"
 #include "Poco/Net/NetSSL.h"
-#include "Poco/Redis/Redis.h"
 #include "Poco/Util/Util.h"
 
 #include "util/datetime.h"
--- codes/c_c++/server/tests/test/poco-shared-library-test.cpp
+++ codes/c_c++/server/tests/test/poco-shared-library-test.cpp
@@ -5,9 +5,8 @@
 #include "Poco/SharedLibrary.h"
 #include "Poco/SHA1Engine.h"
 #include "openssl/sha.h"
-#include "mysql/mysql.h"
 
-GTEST_TEST(PocoSharedLibraryTest, OpenSSL_MySQL)
+GTEST_TEST(PocoSharedLibraryTest, OpenSSL)
 {
     // OpenSSL
     std::array<uint8_t, Poco::SHA1Engine::DIGEST_SIZE> data = {0};
@@ -17,23 +16,17 @@ GTEST_TEST(PocoSharedLibraryTest, OpenSSL_MySQL)
     std::string digest;
     ASSERT_TRUE(common::to_hex_string(data.data(), data.size(), digest, false));
     ASSERT_TRUE(digest == "c12252ceda8be8994d5fa0290a47231c1d16aae3");
-
-    // MySQL
-    ASSERT_TRUE(mysql_get_client_version() == 80033);
 }
 
 GTEST_TEST(PocoSharedLibraryTest, SharedLibrary)
 {
 #if POCO_OS == POCO_OS_WINDOWS_NT
     std::string crypto = "libcrypto-1_1-x64";
-    std::string mysql  = "libmysql";
 #else
     std::string crypto = "libcrypto";
-    std::string mysql  = "libmysqlclient";
 #endif
 
     crypto.append(Poco::SharedLibrary::suffix());
-    mysql.append(Poco::SharedLibrary::suffix());
 
     // OpenSSL
     {
@@ -54,27 +47,6 @@ GTEST_TEST(PocoSharedLibraryTest, SharedLibrary)
         std::string digest;
         ASSERT_TRUE(common::to_hex_string(data.data(), data.size(), digest, false));
         ASSERT_TRUE(digest == "c12252ceda8be8994d5fa0290a47231c1d16aae3");
-
-        ASSERT_TRUE(!sharedlib.hasSymbol("hello_world_not_exist"));
-        ASSERT_THROW(sharedlib.getSymbol("hello_world_not_exist"), Poco::NotFoundException);
-
-        sharedlib.unload();
-        ASSERT_TRUE(!sharedlib.isLoaded());
-    }
-
-    // MySQL
-    {
-        Poco::SharedLibrary sharedlib;
-        ASSERT_TRUE(!sharedlib.isLoaded());
-
-        sharedlib.load(mysql);
-        ASSERT_TRUE(sharedlib.isLoaded() && sharedlib.getPath() == mysql);
-
-        ASSERT_TRUE(sharedlib.hasSymbol("mysql_get_client_version"));
-        using GETV = unsigned long (*)();
-        GETV getv  = reinterpret_cast<GETV>(sharedlib.getSymbol("mysql_get_client_version"));
-
-        ASSERT_TRUE(getv() == 80033);
 
         ASSERT_TRUE(!sharedlib.hasSymbol("hello_world_not_exist"));
         ASSERT_THROW(sharedlib.getSymbol("hello_world_not_exist"), Poco::NotFoundException);
