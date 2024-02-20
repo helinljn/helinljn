c43a699d9512bc47c779f09b7782bd6eccf428ff
helin <helin@wegotime.com>
2024-02-20 20:02:29 +0800

    update brynet/tests/main.cpp

--- codes/c_c++/brynet/tests/main.cpp
+++ codes/c_c++/brynet/tests/main.cpp
@@ -1,20 +1,19 @@
 #include "spdlog.h"
 #include "brynet.h"
 
-using namespace brynet;
+namespace bnet  = brynet::net;
+namespace bbase = brynet::base;
 
 int main(void)
 {
-    auto service = net::IOThreadTcpService::Create();
+    auto service = bnet::IOThreadTcpService::Create();
     service->startWorkerThread(1);
 
-    auto httpEnterCallback = [](const net::http::HTTPParser& httpParser, const net::http::HttpSession::Ptr& session)
+    auto httpEnterCallback = [](const bnet::http::HTTPParser& httpParser, const bnet::http::HttpSession::Ptr& session)
     {
-        std::ignore = httpParser;
+        bnet::http::HttpResponse response;
+        response.setBody(std::string("Welcome to brynet http server!"));
 
-        net::http::HttpResponse response;
-        //std::cout << "method:" << http_method_str(static_cast<http_method>(httpParser.method())) << std::endl;
-        response.setBody(std::string("<html>hello world </html>"));
         if (httpParser.isKeepAlive())
         {
             response.addHeadValue("Connection", "Keep-Alive");
@@ -23,43 +22,35 @@ int main(void)
         else
         {
             response.addHeadValue("Connection", "Close");
-            session->send(response.getResult(), [session]() {
-                session->postShutdown();
-            });
+            session->send(response.getResult(), [session]() {session->postShutdown();});
         }
     };
 
-    auto wsEnterCallback = [](const net::http::HttpSession::Ptr& httpSession,
-                              net::http::WebSocketFormat::WebSocketFrameType opcode,
-                              const std::string& payload)
-    {
-        std::cout << "frame enter of type:" << int(opcode) << std::endl;
-        std::cout << "payload is:" << payload << std::endl;
-        // echo frame
-        httpSession->send(net::http::WebSocketFormat::wsFrameBuild(payload));
-    };
-
-    net::wrapper::HttpListenerBuilder listenBuilder;
+    bnet::wrapper::HttpListenerBuilder listenBuilder;
     listenBuilder
             .WithService(service)
-            .AddSocketProcess([](net::TcpSocket& socket) {
-                socket.setNodelay();
-            })
+            .AddSocketProcess(
+                [](bnet::TcpSocket& socket)
+                {
+                    socket.setNodelay();
+                }
+            )
             .WithMaxRecvBufferSize(1024)
             .WithAddr(false, "0.0.0.0", 8088)
             .WithReusePort()
-            .WithEnterCallback([httpEnterCallback, wsEnterCallback](const net::http::HttpSession::Ptr& httpSession, net::http::HttpSessionHandlers& handlers) {
-                std::ignore = httpSession;
-
-                handlers.setHttpEndCallback(httpEnterCallback);
-                handlers.setWSCallback(wsEnterCallback);
-            })
+            .WithEnterCallback(
+                [httpEnterCallback](const bnet::http::HttpSession::Ptr& httpSession, bnet::http::HttpSessionHandlers& handlers)
+                {
+                    std::ignore = httpSession;
+                    handlers.setHttpEndCallback(httpEnterCallback);
+                }
+            )
             .asyncRun();
 
     while (true)
     {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        if (brynet::base::app_kbhit())
+        if (bbase::app_kbhit())
         {
             break;
         }
