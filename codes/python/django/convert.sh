9e0097c8308f4578fbc691063c597380f49a801b
helin <helinljn@163.com>
2026-04-12 11:09:06 +0800

    update idip_commands.json & django.po

--- codes/python/django/convert.sh
+++ codes/python/django/convert.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 find . -name "*.py"   | xargs dos2unix
+find . -name "*.po"   | xargs dos2unix
 find . -name "*.md"   | xargs unix2dos
 find . -name "*.sql"  | xargs unix2dos
 find . -name "*.html" | xargs unix2dos
