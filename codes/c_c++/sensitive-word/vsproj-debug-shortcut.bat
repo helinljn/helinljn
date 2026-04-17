5fb7880589134876d2c120414ee7e6a51819cd9c
helin <helinljn@163.com>
2026-04-17 12:12:51 +0800

    update vsproj-debug-shortcut.bat

--- codes/c_c++/sensitive-word/vsproj-debug-shortcut.bat
+++ codes/c_c++/sensitive-word/vsproj-debug-shortcut.bat
@@ -4,7 +4,7 @@ set ROOT_DIR=%~dp0
 set SENSITIVE_WORLD_LINK=%ROOT_DIR%\sensitive-word_d.sln
 set SENSITIVE_WORLD_TARGET=%ROOT_DIR%\.build\windows\x64-Debug\sensitive-word.sln
 
-@rem sensitive-word debug shortcut config
+@rem sensitive-word debug shortcut
 if not exist %SENSITIVE_WORLD_LINK% (
     mklink "%SENSITIVE_WORLD_LINK%" "%SENSITIVE_WORLD_TARGET%"
 )
