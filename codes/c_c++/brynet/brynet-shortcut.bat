65055d777942f524baf10e8020c6a1ace76bbd3c
helin <helinljn@163.com>
2026-04-17 12:30:25 +0800

    update *shortcut.bat

--- codes/c_c++/brynet/brynet-shortcut.bat
+++ codes/c_c++/brynet/brynet-shortcut.bat
@@ -4,9 +4,14 @@ set ROOT_DIR=%~dp0
 set BRYNET_LINK=%ROOT_DIR%\brynet_d.sln
 set BRYNET_TARGET=%ROOT_DIR%\.build\windows\x64-Debug\brynet.sln
 
-@rem brynet_d
-if not exist %BRYNET_LINK% (
-    mklink "%BRYNET_LINK%" "%BRYNET_TARGET%"
+@rem brynet debug shortcut
+whoami /priv | findstr /i "SeSecurityPrivilege" >nul 2>&1
+if %errorlevel% equ 0 (
+    if not exist %BRYNET_LINK% (
+        mklink "%BRYNET_LINK%" "%BRYNET_TARGET%"
+    )
+) else (
+    echo Please run as administrator privileges.
 )
 
 pause
--- codes/c_c++/sensitive-word/vsproj-debug-shortcut.bat
+++ codes/c_c++/sensitive-word/vsproj-debug-shortcut.bat
@@ -5,8 +5,13 @@ set SENSITIVE_WORLD_LINK=%ROOT_DIR%\sensitive-word_d.sln
 set SENSITIVE_WORLD_TARGET=%ROOT_DIR%\.build\windows\x64-Debug\sensitive-word.sln
 
 @rem sensitive-word debug shortcut
-if not exist %SENSITIVE_WORLD_LINK% (
-    mklink "%SENSITIVE_WORLD_LINK%" "%SENSITIVE_WORLD_TARGET%"
+whoami /priv | findstr /i "SeSecurityPrivilege" >nul 2>&1
+if %errorlevel% equ 0 (
+    if not exist %SENSITIVE_WORLD_LINK% (
+        mklink "%SENSITIVE_WORLD_LINK%" "%SENSITIVE_WORLD_TARGET%"
+    )
+) else (
+    echo Please run as administrator privileges.
 )
 
 pause
