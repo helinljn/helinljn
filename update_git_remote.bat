054a16bedc1038ab6e5de3e8a5ad970275594177
helin <helinljn@163.com>
2026-05-13 15:50:57 +0800

    update VSCode.txt

--- update_git_remote.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-git remote set-url origin https://github.com/helinljn/helinljn.git
-@rem git remote set-url origin https://kkgithub.com/helinljn/helinljn.git
-git remote -v
-
-pause
--- update_git_remote.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git remote set-url origin https://github.com/helinljn/helinljn.git
-#git remote set-url origin https://kkgithub.com/helinljn/helinljn.git
-git remote -v
