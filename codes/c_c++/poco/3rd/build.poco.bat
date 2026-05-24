c0640f160222a58d9254bdeee05244ab042ccfba
helin <helinljn@163.com>
2026-05-24 22:33:40 +0800

    update poco build scripts

--- codes/c_c++/poco/3rd/build.poco.bat
+++ codes/c_c++/poco/3rd/build.poco.bat
@@ -1,96 +1,45 @@
 @echo off
-setlocal enabledelayedexpansion
 
 set VS_VERSION="Visual Studio 17 2022"
 set ROOT_DIR=%~dp0
 set DEBUG_DIR=%ROOT_DIR%/poco/.build/windows/x64-Debug
 set RELEASE_DIR=%ROOT_DIR%/poco/.build/windows/x64-Release
-set POCO_MODULES=Foundation Encodings XML JSON Util Net NetSSL Crypto Data DataSQLite DataMySQL Redis JWT Prometheus Zip
 
-goto :main
+if %1 equ debug (
+    mkdir "%DEBUG_DIR%" 2>nul
+    cd %DEBUG_DIR%
 
-:build_poco_config
-set "POCO_CONFIG=%~1"
-set "POCO_BUILD_DIR=%~2"
+    cmake -G %VS_VERSION% -A x64 -C %ROOT_DIR%poco.cmake -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug ../../../
+    cmake --build . --config Debug
 
-mkdir "%POCO_BUILD_DIR%" 2>nul
-pushd "%POCO_BUILD_DIR%" || exit /b 1
+    cd %ROOT_DIR%
+) else if %1 equ release (
+    mkdir "%RELEASE_DIR%" 2>nul
+    cd %RELEASE_DIR%
 
-cmake -G %VS_VERSION% -A x64 -C "%ROOT_DIR%poco.cmake" -DCMAKE_CONFIGURATION_TYPES=%POCO_CONFIG% -DCMAKE_BUILD_TYPE=%POCO_CONFIG% ../../../
-if errorlevel 1 (
-    popd
-    exit /b 1
-)
+    cmake -G %VS_VERSION% -A x64 -C %ROOT_DIR%poco.cmake -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_BUILD_TYPE=Release ../../../
+    cmake --build . --config Release
 
-cmake --build . --config %POCO_CONFIG%
-if errorlevel 1 (
-    popd
-    exit /b 1
-)
+    cd %ROOT_DIR%
+) else if %1 equ all (
+    mkdir "%DEBUG_DIR%" 2>nul
+    cd %DEBUG_DIR%
 
-popd
-
-call :install_poco_config "%POCO_CONFIG%" "%POCO_BUILD_DIR%"
-exit /b %ERRORLEVEL%
-
-:install_poco_config
-set "POCO_CONFIG=%~1"
-set "POCO_BUILD_DIR=%~2"
-set "POCO_BIN_DIR=%POCO_BUILD_DIR%\bin"
-set "POCO_LIB_DIR=%POCO_BUILD_DIR%\lib"
-
-if /I "%POCO_CONFIG%"=="Debug" (
-    set "POCO_SUFFIX=d"
-) else (
-    set "POCO_SUFFIX="
-)
-
-mkdir "%POCO_LIB_DIR%" 2>nul
-
-for %%M in (%POCO_MODULES%) do (
-    call :copy_required "%POCO_BIN_DIR%\Poco%%M%POCO_SUFFIX%.dll" "%POCO_LIB_DIR%"
-    if errorlevel 1 exit /b 1
-
-    call :copy_required "%POCO_BIN_DIR%\Poco%%M%POCO_SUFFIX%.lib" "%POCO_LIB_DIR%"
-    if errorlevel 1 exit /b 1
-
-    if /I "%POCO_CONFIG%"=="Debug" (
-        call :copy_required "%POCO_BIN_DIR%\Poco%%M%POCO_SUFFIX%.pdb" "%POCO_LIB_DIR%"
-        if errorlevel 1 exit /b 1
-    )
-)
-
-exit /b 0
-
-:copy_required
-if not exist "%~1" (
-    echo Missing Poco build artifact: %~1
-    exit /b 1
-)
-
-copy /Y "%~1" "%~2\" >nul
-if errorlevel 1 exit /b 1
-
-exit /b 0
-
-:main
-if /I "%~1"=="debug" (
-    call :build_poco_config "Debug" "%DEBUG_DIR%"
-    if errorlevel 1 exit /b 1
-) else if /I "%~1"=="release" (
-    call :build_poco_config "Release" "%RELEASE_DIR%"
-    if errorlevel 1 exit /b 1
-) else if /I "%~1"=="all" (
-    call :build_poco_config "Debug" "%DEBUG_DIR%"
-    if errorlevel 1 exit /b 1
+    cmake -G %VS_VERSION% -A x64 -C %ROOT_DIR%poco.cmake -DCMAKE_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug ../../../
+    cmake --build . --config Debug
     echo.
     echo.
     echo.
 
-    call :build_poco_config "Release" "%RELEASE_DIR%"
-    if errorlevel 1 exit /b 1
+    cd %ROOT_DIR%
+
+    mkdir "%RELEASE_DIR%" 2>nul
+    cd %RELEASE_DIR%
+
+    cmake -G %VS_VERSION% -A x64 -C %ROOT_DIR%poco.cmake -DCMAKE_CONFIGURATION_TYPES=Release -DCMAKE_BUILD_TYPE=Release ../../../
+    cmake --build . --config Release
+
+    cd %ROOT_DIR%
 ) else (
     echo "Usage: build.poco.bat [debug | release | all]"
 )
-
-exit /b 0
--- codes/c_c++/poco/3rd/build.poco.sh
+++ codes/c_c++/poco/3rd/build.poco.sh
@@ -1,78 +1,44 @@
 #!/bin/bash
 
-ROOT_DIR=$(cd "$(dirname "$0")" && pwd)
-DEBUG_DIR="$ROOT_DIR/poco/.build/linux/x64-Debug"
-RELEASE_DIR="$ROOT_DIR/poco/.build/linux/x64-Release"
-POCO_MODULES=(Foundation Encodings XML JSON Util Net NetSSL Crypto Data DataSQLite DataMySQL Redis JWT Prometheus Zip)
+ROOT_DIR=`pwd`
+DEBUG_DIR=$ROOT_DIR/poco/.build/linux/x64-Debug
+RELEASE_DIR=$ROOT_DIR/poco/.build/linux/x64-Release
 
-build_poco_config() {
-    local POCO_CONFIG=$1
-    local POCO_BUILD_DIR=$2
+if   [ "$1" = "debug" ]; then
+    mkdir -p $DEBUG_DIR
+    cd $DEBUG_DIR
 
-    mkdir -p "$POCO_BUILD_DIR" || exit 1
-    cd "$POCO_BUILD_DIR" || exit 1
+    cmake -C $ROOT_DIR/poco.cmake -DCMAKE_BUILD_TYPE=Debug ../../../
+    cmake --build . --config Debug -j $(nproc)
 
-    cmake -C "$ROOT_DIR/poco.cmake" -DCMAKE_BUILD_TYPE="$POCO_CONFIG" ../../../
-    if [ $? -ne 0 ]; then exit 1; fi
-
-    cmake --build . --config "$POCO_CONFIG" --parallel 16
-    if [ $? -ne 0 ]; then exit 1; fi
-
-    install_poco_config "$POCO_CONFIG" "$POCO_BUILD_DIR" || exit 1
-
-    cd "$ROOT_DIR" || exit 1
-}
-
-install_poco_config() {
-    local POCO_CONFIG=$1
-    local POCO_BUILD_DIR=$2
-    local POCO_BIN_DIR="$POCO_BUILD_DIR/bin"
-    local POCO_LIB_DIR="$POCO_BUILD_DIR/lib"
-    local POCO_SUFFIX=
-
-    if [ "$POCO_CONFIG" = "Debug" ]; then
-        POCO_SUFFIX=d
-    fi
-
-    mkdir -p "$POCO_LIB_DIR" || return 1
-
-    for module in "${POCO_MODULES[@]}"; do
-        copy_required "$POCO_BIN_DIR/libPoco${module}${POCO_SUFFIX}.so" "$POCO_LIB_DIR" || return 1
-        copy_required "$POCO_BIN_DIR/libPoco${module}${POCO_SUFFIX}.so.123" "$POCO_LIB_DIR" || return 1
-    done
-}
-
-copy_required() {
-    local POCO_SOURCE_FILE=$1
-    local POCO_TARGET_DIR=$2
-    local POCO_TARGET_FILE="$POCO_TARGET_DIR/$(basename "$POCO_SOURCE_FILE")"
-
-    if [ ! -e "$POCO_SOURCE_FILE" ] && [ ! -L "$POCO_SOURCE_FILE" ]; then
-        echo "Missing Poco build artifact: $POCO_SOURCE_FILE"
-        return 1
-    fi
-
-    if [ -L "$POCO_SOURCE_FILE" ]; then
-        local POCO_LINK_TARGET
-        POCO_LINK_TARGET=$(readlink "$POCO_SOURCE_FILE") || return 1
-        rm -f "$POCO_TARGET_FILE" || return 1
-        ln -s "$POCO_LINK_TARGET" "$POCO_TARGET_FILE" || return 1
-    else
-        cp -f "$POCO_SOURCE_FILE" "$POCO_TARGET_FILE" || return 1
-    fi
-}
-
-if [ "$1" = "debug" ]; then
-    build_poco_config Debug "$DEBUG_DIR"
+    cd $ROOT_DIR
 elif [ "$1" = "release" ]; then
-    build_poco_config Release "$RELEASE_DIR"
+    mkdir -p $RELEASE_DIR
+    cd $RELEASE_DIR
+
+    cmake -C $ROOT_DIR/poco.cmake -DCMAKE_BUILD_TYPE=Release ../../../
+    cmake --build . --config Release -j $(nproc)
+
+    cd $ROOT_DIR
 elif [ "$1" = "all" ]; then
-    build_poco_config Debug "$DEBUG_DIR"
+    mkdir -p $DEBUG_DIR
+    cd $DEBUG_DIR
+
+    cmake -C $ROOT_DIR/poco.cmake -DCMAKE_BUILD_TYPE=Debug ../../../
+    cmake --build . --config Debug -j $(nproc)
     echo ""
     echo ""
     echo ""
 
-    build_poco_config Release "$RELEASE_DIR"
+    cd $ROOT_DIR
+
+    mkdir -p $RELEASE_DIR
+    cd $RELEASE_DIR
+
+    cmake -C $ROOT_DIR/poco.cmake -DCMAKE_BUILD_TYPE=Release ../../../
+    cmake --build . --config Release -j $(nproc)
+
+    cd $ROOT_DIR
 else
     echo "Usage: ./build.poco.sh [debug | release | all]"
 fi
